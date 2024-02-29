20dff48db25e90f155ca279e2b59588c063fb80b
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-02-28 23:24:14 -0300

    edited tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
+        white: "#ffffff",
+        grey: "#808080",
+        black: "#121212",
+        yellow: "#f4d04e",
       },
       fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
+        figtree: ["Figtree", "sans-serif"],
       },
     },
     screens: {
